e1f887b940436002107d06950c33bd162e23a43c
gsentsas <gsentransport@gmail.com>
2026-08-20 21:05:57 +0000

    test(shop): load shipping management tests

--- odoo/custom-addons/dally_shop/tests/__init__.py
+++ odoo/custom-addons/dally_shop/tests/__init__.py
@@ -11,3 +11,4 @@ from . import test_order_workflow
 from . import test_delivery
 from . import test_confirmation_guard
 from . import test_delivery_immutability
+from . import test_shipping_management

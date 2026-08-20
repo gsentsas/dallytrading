145a0438f6adeace3d42342c955e43d227bd6542
gsentsas <gsentransport@gmail.com>
2026-08-20 19:36:39 +0000

    feat(shop): load workflow checkout projection

--- odoo/custom-addons/dally_shop/models/__init__.py
+++ odoo/custom-addons/dally_shop/models/__init__.py
@@ -3,5 +3,6 @@ from . import shop_product_image
 from . import product_template
 from . import shop_order
 from . import shop_order_workflow
+from . import shop_order_workflow_projection
 from . import shop_order_portal
 from . import res_config_settings

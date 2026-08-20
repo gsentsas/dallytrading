533d8f6f500c88de48d13f78586b99413cf78cf0
gsentsas <gsentransport@gmail.com>
2026-08-20 20:13:26 +0000

    feat(shop): load delivery model

--- odoo/custom-addons/dally_shop/models/__init__.py
+++ odoo/custom-addons/dally_shop/models/__init__.py
@@ -2,6 +2,7 @@ from . import dally_shop_category
 from . import shop_product_image
 from . import product_template
 from . import shop_order
+from . import shop_delivery
 from . import shop_order_workflow
 from . import shop_order_workflow_projection
 from . import shop_order_portal

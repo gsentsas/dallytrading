ed23c4882a25542357da7ed7c0664c9e93328d1b
gsentsas <gsentransport@gmail.com>
2026-08-21 07:50:03 +0000

    test(freight): register payment suites

--- odoo/custom-addons/dally_freight_billing/tests/__init__.py
+++ odoo/custom-addons/dally_freight_billing/tests/__init__.py
@@ -4,3 +4,5 @@ from . import test_freight_sync
 from . import test_freight_sync_endpoint
 from . import test_freight_invoice
 from . import test_freight_invoice_endpoint
+from . import test_freight_payment
+from . import test_freight_payment_endpoint

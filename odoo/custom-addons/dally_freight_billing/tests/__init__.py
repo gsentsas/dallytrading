8b5a5710fdf0a096a2b0aa8f5c1ace8299123d83
gsentsas <gsentransport@gmail.com>
2026-08-21 07:35:17 +0000

    test(freight-billing): load native invoice tests

--- odoo/custom-addons/dally_freight_billing/tests/__init__.py
+++ odoo/custom-addons/dally_freight_billing/tests/__init__.py
@@ -2,3 +2,5 @@
 from . import test_freight_tariff
 from . import test_freight_sync
 from . import test_freight_sync_endpoint
+from . import test_freight_invoice
+from . import test_freight_invoice_endpoint

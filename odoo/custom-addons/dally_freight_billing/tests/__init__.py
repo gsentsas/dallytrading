9b8c5b36d1aa61446f8b6fe068994d9ff9ebd012
gsentsas <gsentransport@gmail.com>
2026-08-21 09:08:34 +0000

    test(freight): load cash operation tests

--- odoo/custom-addons/dally_freight_billing/tests/__init__.py
+++ odoo/custom-addons/dally_freight_billing/tests/__init__.py
@@ -7,3 +7,5 @@ from . import test_freight_invoice
 from . import test_freight_invoice_endpoint
 from . import test_freight_payment
 from . import test_freight_payment_endpoint
+from . import test_cash_operations
+from . import test_cash_endpoint

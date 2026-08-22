01b16132ad6fbcd282445b1b0a387b2a3b4f9a73
gsentsas <gsentransport@gmail.com>
2026-08-22 18:21:08 +0000

    test(freight): load payment reconcile tests

--- odoo/custom-addons/dally_freight_billing/tests/__init__.py
+++ odoo/custom-addons/dally_freight_billing/tests/__init__.py
@@ -7,6 +7,7 @@ from . import test_freight_invoice
 from . import test_freight_invoice_endpoint
 from . import test_freight_payment
 from . import test_freight_payment_endpoint
+from . import test_freight_payment_reconcile
 from . import test_cash_operations
 from . import test_cash_endpoint
 from . import test_freight_invoice_report

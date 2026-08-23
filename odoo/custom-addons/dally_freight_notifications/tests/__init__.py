295f2644e5c6c4cb9dfe536b34bc426213a3423d
gsentsas <gsentransport@gmail.com>
2026-08-23 20:37:52 +0000

    test(notifications): load tracking link snapshot regression

--- odoo/custom-addons/dally_freight_notifications/tests/__init__.py
+++ odoo/custom-addons/dally_freight_notifications/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_state_policy
 from . import test_email_delivery
 from . import test_notification_ops
 from . import test_policy_visibility_refresh
+from . import test_tracking_link_snapshot

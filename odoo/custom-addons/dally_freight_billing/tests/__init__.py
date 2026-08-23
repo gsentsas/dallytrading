5beb4aba63399e7d801da1dcb3823ea4aee08577
gsentsas <gsentransport@gmail.com>
2026-08-23 18:33:49 +0000

    test(freight): load tracking ACL regression test

--- odoo/custom-addons/dally_freight_billing/tests/__init__.py
+++ odoo/custom-addons/dally_freight_billing/tests/__init__.py
@@ -2,6 +2,7 @@
 from . import test_freight_tariff
 from . import test_freight_sync
 from . import test_freight_sync_endpoint
+from . import test_freight_sync_tracking_acl
 from . import test_freight_locked_sync
 from . import test_freight_invoice
 from . import test_freight_invoice_endpoint

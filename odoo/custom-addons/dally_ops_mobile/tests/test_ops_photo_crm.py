029160ab187ded6551270c7932f28854b541e3ac
gsentsas <gsentransport@gmail.com>
2026-09-10 11:21:48 +0000

    test(crm): harden Ops photo visibility boundaries

--- odoo/custom-addons/dally_ops_mobile/tests/test_ops_photo_crm.py
+++ odoo/custom-addons/dally_ops_mobile/tests/test_ops_photo_crm.py
@@ -14,6 +14,7 @@ class TestOpsPhotoCRM(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.company = cls.env.company
+        cls.other_company = cls.env["res.company"].create({"name": "CRM Photo Autre"})
         cls.partner = cls.env["res.partner"].create({
             "name": "Client photo CRM",
             "company_id": cls.company.id,
@@ -43,6 +44,36 @@ class TestOpsPhotoCRM(TransactionCase):
             "operator_user_id": cls.env.user.id,
         })
         cls.attachment.write({"res_id": cls.photo.id})
+
+        cls.other_partner = cls.env["res.partner"].sudo().create({
+            "name": "Client photo autre société",
+            "company_id": cls.other_company.id,
+        })
+        cls.other_shipment = cls.env["dally.shipment"].sudo().create({
+            "partner_id": cls.other_partner.id,
+            "company_id": cls.other_company.id,
+            "external_reference": "OPS-PHOTO-CRM-OTHER",
+            "transport_mode": "air",
+            "direction": "export",
+        })
+        cls.other_attachment = cls.env["ir.attachment"].sudo().create({
+            "name": "autre.jpg",
+            "raw": b"other-company-photo",
+            "mimetype": "image/jpeg",
+            "res_model": "dally.ops.photo",
+            "res_id": 0,
+            "company_id": cls.other_company.id,
+            "public": False,
+        })
+        cls.other_photo = cls.env["dally.ops.photo"].sudo().create({
+            "photo_uuid": str(uuid.uuid4()),
+            "company_id": cls.other_company.id,
+            "shipment_id": cls.other_shipment.id,
+            "attachment_id": cls.other_attachment.id,
+            "kind": "reception",
+            "operator_user_id": cls.env.user.id,
+        })
+        cls.other_attachment.write({"res_id": cls.other_photo.id})
 
         cls.crm_user = cls.env["res.users"].with_context(
             no_reset_password=True).create({
@@ -78,6 +109,21 @@ class TestOpsPhotoCRM(TransactionCase):
             ("res_model", "=", "dally.ops.photo"),
             ("res_id", "=", self.photo.id),
         ]), 1)
+
+    def test_removed_photo_is_not_shown_on_shipment(self):
+        self.photo.sudo().write({"active": False})
+        shipment = self.shipment.with_user(self.crm_user)
+        self.assertFalse(shipment.ops_photo_ids)
+
+    def test_other_company_photo_is_hidden(self):
+        found = self.env["dally.ops.photo"].with_user(self.crm_user).search([
+            ("id", "=", self.other_photo.id),
+        ])
+        self.assertFalse(found)
+
+    def test_crm_reader_cannot_modify_ops_photo(self):
+        with self.assertRaises(AccessError):
+            self.photo.with_user(self.crm_user).write({"kind": "package"})
 
     def test_portal_cannot_read_ops_photo(self):
         with self.assertRaises(AccessError):

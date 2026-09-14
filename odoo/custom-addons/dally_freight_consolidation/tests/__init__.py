617be662ce5da670cff27a0058b76afa4206cf90
gsentsas <gsentransport@gmail.com>
2026-09-14 16:10:44 +0000

    test(consolidation): cover supplementary invoice button

--- odoo/custom-addons/dally_freight_consolidation/tests/__init__.py
+++ odoo/custom-addons/dally_freight_consolidation/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import test_consolidation_workflow
 from . import test_consolidation_reference_guard
+from . import test_consolidation_invoice_button
 from . import test_line_and_packages
 from . import test_historical_backfill
 from . import test_departure_completeness
